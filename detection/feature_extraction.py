726354e1a2fa90c69f005590f731848d5d64bd95
Andrew Tran <andrewalvintran@gmail.com>
2017-08-22 01:34:29 -0700

    Extract features from each color channel

--- detection/feature_extraction.py
+++ detection/feature_extraction.py
@@ -1,3 +1,4 @@
+import numpy as np
 from skimage.feature import hog
 
 
@@ -14,3 +15,18 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block,
     else:
         features = hog(img, **hog_args)
         return features
+
+
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+    """Computes the color histogram features
+    For each channel a histogram is computed and concatenated into a single
+    feature vector
+    """
+    num_channels = 3
+    channels_hist = (np.histogram(img[:, :, i], bins=nbins, range=bins_range) for i in range(num_channels))
+    channel1_hist, channel2_hist, channel3_hist = channels_hist
+
+    # Generating bin centers
+    hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+
+    return hist_features
