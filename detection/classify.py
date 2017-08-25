6c3269b7b627cd8588629098a69c4fcb552525b2
Andrew Tran <andrewalvintran@gmail.com>
2017-08-24 23:47:37 -0700

    Search for vehicles in a sliding window

--- detection/classify.py
+++ detection/classify.py
@@ -1,10 +1,16 @@
 import numpy as np
+import os
 import time
+from sklearn.externals import joblib
 from sklearn.svm import LinearSVC
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from detection import utils
 from detection import feature_extraction as fe
+
+
+CLF_PATH = "../data/svm_model.pkl"
+SCALER_PATH = "../data/scaler.pkl"
 
 
 def create_model(data):
@@ -18,6 +24,7 @@ def create_model(data):
 
     print(round(t2 - t, 2), 'seconds to train SVC...')
     print("Test accuracy of SVC = ", svc.score(x_test, y_test))
+    joblib.dump(svc, CLF_PATH)
     return svc
 
 
@@ -35,33 +42,50 @@ def process_data_for_train(car_features, notcar_features):
     rand_state = np.random.randint(0, 100)
     x_train, x_test, y_train, y_test = train_test_split(scaled_x, y, test_size=0.2,
                                                         random_state = rand_state)
-
-    return {"train": [x_train, y_train],
-            "test": [x_test, y_test]}
+    joblib.dump(x_scaler, SCALER_PATH)
+    return (x_scaler,
+           {"train": [x_train, y_train],
+            "test": [x_test, y_test]})
 
 
 def main():
     vehicle_dir = '../data/vehicles/*/'
     vehicle_imgs = utils.get_images(vehicle_dir)
-
     cars = list(vehicle_imgs)
+
     non_vehicle_dir = '../data/non-vehicles/*/'
     non_vehicle_imgs = utils.get_images(non_vehicle_dir)
     notcars = list(non_vehicle_imgs)
 
-    car_features = fe.extract_features(cars, cspace='RGB', spatial_size=(32, 32),
-                                    hist_bins=32, hist_range=(0, 256), hog_feat=False)
-    notcar_features = fe.extract_features(notcars, cspace='RGB', spatial_size=(32, 32),
-                                       hist_bins=32, hist_range=(0, 256), hog_feat=False)
+    spatial_size = (32, 32)
+    hist_bins = 32
+    hog_channel = 'ALL'
 
-    data = process_data_for_train(car_features, notcar_features)
-    clf = create_model(data)
+    if os.path.exists(CLF_PATH):
+        clf = joblib.load(CLF_PATH)
+        scaler = joblib.load(SCALER_PATH)
+    else:
+        car_features = fe.extract_features(cars, hog_channel=hog_channel, spatial_size=spatial_size,
+                                           hist_bins=hist_bins)
+        notcar_features = fe.extract_features(notcars, hog_channel=hog_channel, spatial_size=spatial_size,
+                                              hist_bins=hist_bins)
 
-    car_features = fe.extract_features(cars, hog_channel='ALL')
-    notcar_features= fe.extract_features(notcars, hog_channel='ALL')
+        scaler, data = process_data_for_train(car_features, notcar_features)
+        clf = create_model(data)
 
-    data_hog = process_data_for_train(car_features, notcar_features)
-    clf_all_features = create_model(data_hog)
+    test_images = list(utils.get_images("../data/test_images", type='jpg'))
+    test_image = test_images[0]
+    windows = utils.slide_windows(test_image, xy_window=(96, 96), y_start=350)
+
+    hot_windows = utils.search_windows(test_image, windows, clf, scaler,
+                                       spatial_size=spatial_size, hist_bins=hist_bins, hog_channel=hog_channel)
+
+    draw_image = np.copy(test_image)
+    window_img = utils.draw_boxes(draw_image, hot_windows)
+
+    from matplotlib import pyplot as plt
+    plt.imshow(window_img)
+    plt.show()
 
 
 if __name__ == "__main__":
--- detection/utils.py
+++ detection/utils.py
@@ -1,5 +1,7 @@
 import cv2
 import glob
+import numpy as np
+from detection import feature_extraction as fe
 
 
 def get_images(dir, type='png'):
@@ -9,6 +11,90 @@ def get_images(dir, type='png'):
 
     images = (img for img in glob.glob(dir + "/*." + type))
     return (cv2.cvtColor(cv2.imread(img), cv2.COLOR_BGR2RGB) for img in images)
+
+
+def draw_boxes(img, bboxes, *, color=(0, 0, 255), thick=6):
+    """Draws rectangles for the given bounding boxes coordinates"""
+    imcopy = np.copy(img)
+
+    for bbox in bboxes:
+        cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
+
+    return imcopy
+
+
+def slide_windows(img, *, x_start=None, x_stop=None, y_start=None, y_stop=None,
+                  xy_window=(64, 64), xy_overlap=(0.5, 0.5)):
+    """Return a list of windows to search over"""
+    x, y = img.shape[1], img.shape[0]
+
+    # If start and end points are not specified, use the entire image
+    if x_start is None:
+        x_start = 0
+    if x_stop is None:
+        x_stop = x
+    if y_start is None:
+        y_start = 0
+    if y_stop is None:
+        y_stop = y
+
+    # Compute span of region
+    x_span = x_stop - x_start
+    y_span = y_stop - y_start
+
+    # Number of pixels per step along x and y direction
+    nx_pix_per_step = np.int(xy_window[0]*(1 - xy_overlap[0]))
+    ny_pix_per_step = np.int(xy_window[1]*(1- xy_overlap[1]))
+
+    # Compute number of windows in the x and y direction
+    nx_buffer = np.int(xy_window[0] * xy_overlap[0])
+    ny_buffer = np.int(xy_window[1] * xy_overlap[1])
+    nx_windows = np.int((x_span - nx_buffer)/nx_pix_per_step)
+    ny_windows = np.int((y_span - ny_buffer)/ny_pix_per_step)
+
+    window_list = []
+
+    for ys in range(ny_windows):
+        for xs in range(nx_windows):
+            startx = xs * nx_pix_per_step + x_start
+            endx = startx + xy_window[0]
+            starty = ys * ny_pix_per_step + y_start
+            endy = starty + xy_window[1]
+
+            window_list.append(((startx, starty), (endx, endy)))
+
+    return window_list
+
+
+def search_windows(img, windows, clf, scaler, *, color_space='RGB',
+                   spatial_size=(32, 32), hist_bins=32, hist_range=(0, 256),
+                   orient=9, pix_per_cell=8, cell_per_block=2, hog_channel=0,
+                   spatial_feat=True, hist_feat=True, hog_feat=True):
+    """
+    Given an image and a list of windows to search, the function searches
+    over these windows to classify the images and return positive results as a 
+    list of window coordinates
+    """
+    on_windows = []
+    for window in windows:
+        # Extract the test window from original image
+        test_img = cv2.resize(img[window[0][1]:window[1][1], window[0][0]:window[1][0]], (64, 64))
+
+        features = fe.extract_features(
+                            [test_img], cspace=color_space,
+                            spatial_size=spatial_size, hist_bins=hist_bins,
+                            hist_range=hist_range,
+                            orient=orient, pix_per_cell=pix_per_cell,
+                            cell_per_block=cell_per_block,
+                            hog_channel=hog_channel, spatial_feat=spatial_feat,
+                            hist_feat=hist_feat, hog_feat=hog_feat)
+
+        test_features = scaler.transform(np.array(features[0]).reshape(1, -1))
+        prediction = clf.predict(test_features)
+        if prediction == 1:
+            on_windows.append(window)
+
+    return on_windows
 
 
 if __name__ == "__main__":
